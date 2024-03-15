82bf744ba53ade9105ac9f2754052b6283299ce0
Ankur Datta <datta@prisma.io>
2024-03-15 19:09:40 +0600

    remove: accelerate extension

--- netlify/edge-functions/hello.ts
+++ netlify/edge-functions/hello.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "../../generated/client/deno/edge.ts";
-import { withAccelerate } from "npm:@prisma/extension-accelerate@1.0.0";
+// import { withAccelerate } from "npm:@prisma/extension-accelerate@1.0.0";
 import { Hono } from "https://deno.land/x/hono/mod.ts";
 import { handle } from "https://deno.land/x/hono/adapter/netlify/mod.ts";
 import type { Env } from "https://deno.land/x/hono/adapter/netlify/mod.ts";
@@ -9,16 +9,11 @@ const app = new Hono<Env>();
 app.get("/hello", async (context) => {
   const prisma = new PrismaClient({
     datasourceUrl: Deno.env.get("DATABASE_URL"),
-  }).$extends(withAccelerate());
+  });
 
-  const result = await prisma.person
-    .findMany({
-      take: 10,
-      cacheStrategy: {
-        ttl: 120,
-      },
-    })
-    .withAccelerateInfo();
+  const result = await prisma.person.findMany({
+    take: 10,
+  });
 
   return context.json({
     ...result,
